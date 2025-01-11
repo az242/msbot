3bae03fdf2e02a79b775fe59b56b92c2ffe1965f
Andrew Zhu <andrewzhu@Andrews-MacBook-Air.local>
2025-01-10 20:11:53 -0500

    added more stuff

--- src/servers/Server.java
+++ src/servers/Server.java
@@ -10,6 +10,7 @@ import javax.imageio.ImageIO;
 public abstract class Server {
 	public String name;
 	
+	// area that is the play area of the client (minus the application title bar)
 	public Rectangle searchArea;
 	
 	public Rectangle nameSearchArea;
@@ -20,19 +21,25 @@ public abstract class Server {
 	public Rectangle chatExpanderSearchArea;
 	public Rectangle minimapNameSearchArea;
 	
-	//for finding clients
+	//client icon search area (in application title bar)
 	public Rectangle clientIconSearchArea;
+	//taskbar icon search area
 	public Rectangle taskbarIconSearchArea;
+	//client adjustment to exclude the title bar
 	public Rectangle clientCaptureAdjustment;
+	//taskbar icon image
 	public BufferedImage taskBarIcon;
 	
 	public BufferedImage[] numImages;
 	public BufferedImage[] levelImages;
 	public BufferedImage[] mesosNumImages;
 	
+	//search area for the inventory 
 	public Rectangle inventorySearchArea;
+	//inventory label image
 	public BufferedImage inventoryLabel;
-	public Rectangle inventoryLabelSearchArea;
+	public Rectangle inventoryLabelSearchArea; // do i need this?
+	//search areas for mesos
 	public Rectangle billionmesos;
 	public Rectangle millionmesos;
 	public Rectangle thousandmesos;
